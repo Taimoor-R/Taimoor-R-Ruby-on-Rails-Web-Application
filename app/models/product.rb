40d284bcd19b99c3aac67b2ecff4ecdc810df82d
Taimoor Rizwan <tr00564@surrey.ac.uk>
2021-11-19 23:30:14 +0000

    Testing for linr item and cart added and tested

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :supply, :description, :image_url, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.0, less_than_or_equal_to:1000.00}
   validates :supply, numericality: {greater_than_or_equal_to: 1}
@@ -7,4 +10,12 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png|jpeg)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
 }
+  private
+# ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
