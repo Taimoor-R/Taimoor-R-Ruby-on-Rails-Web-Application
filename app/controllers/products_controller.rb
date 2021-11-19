6b64684ef5c19ffc8935f82f1e7e2f1faddbe70b
Taimoor Rizwan <tr00564@surrey.ac.uk>
2021-11-19 03:26:08 +0000

    Category pages made and working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,6 +64,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price, :supply)
+      params.require(:product).permit(:title, :description, :image_url, :price, :supply, :category)
     end
 end
