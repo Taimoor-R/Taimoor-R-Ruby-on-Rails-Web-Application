9e588fbacc600760bda144006c76eb98325e7454
Taimoor Rizwan <tr00564@surrey.ac.uk>
2021-11-24 01:26:58 +0000

    Made a reation between user model and cart but dont understand why it isnt showing specfic page for user cart and remove belongsto and has one reations in cart.rb and user.rb everything works fine

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_220314) do
+ActiveRecord::Schema.define(version: 2021_11_23_235502) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "catagories", force: :cascade do |t|
