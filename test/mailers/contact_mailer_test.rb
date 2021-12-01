ff7a2282010cda7cd634bd0ffd6dd013ffb97fbb
Taimoor Rizwan <tr00564@surrey.ac.uk>
2021-12-01 00:21:33 +0000

    Added mailer preview as well as contact_mailer action

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -4,4 +4,10 @@ class ContactMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should return contact email" do
+    mail = ContactMailer.contact_email("timmy@me.com",
+      "Timmy Tommorow", "1234567890", @message = "Hello")
+      assert_equal ['info@MCMWebstore.com'], mail.to
+      assert_equal ['info@MCMWebstore.com'], mail.from
+    end
 end
