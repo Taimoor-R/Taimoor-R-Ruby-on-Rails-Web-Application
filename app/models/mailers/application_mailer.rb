53b342bb286a6ee4aa30dc036bbe74dbdf8ce6bd
Taimoor Rizwan <tr00564@surrey.ac.uk>
2021-12-06 22:56:18 +0000

    Added vaildation for mailer and tweaked some errors

--- app/models/mailers/application_mailer.rb
+++ app/models/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  validates :name,:email,:telephone,:message, presence: true 
   default from: 'from@example.com' #sets defalut email from specified email
   layout 'mailer'
 end
